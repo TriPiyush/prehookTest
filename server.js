d52892e5407bf80e3e566d93cd3ff32159f57176
madhumitakhutia <madhumita.k.influxiq@gmail.com>
2025-04-24 12:46:42 +0530

    changes

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello from server!\n');
 });
 
 const PORT = 3000;
 
-
 server.listen(PORT, () => {
-        console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
